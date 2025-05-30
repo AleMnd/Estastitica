e725f96a2201cc9a7777816710dee35adee12894
AleMnd <mendes.alessandro34@gmail.com>
2025-05-30 19:18:47 -0300

    Tirando valores de freq e colocando a Media

--- Estatistica/JS/Exp.js
+++ Estatistica/JS/Exp.js
@@ -11,22 +11,19 @@
   
     function maior() {
         a = parseFloat(document.getElementById('a').value);
-        tudo = parseFloat(document.getElementById('tudo').value);
         n = parseFloat(document.getElementById('n').value);
         let resultado = "";
-      if (isNaN(a)|| isNaN (tudo) || isNaN (n)) {
+      if (isNaN(a)||isNaN (n)) {
       alert("Insira os valores");
       } else {
-        media = (tudo / n);
-        lambda = (1 / media);
+        lambda = (1 / n);
         probabilidade = arredondar2(Math.exp(-lambda * a) * 100);
 
         resultado = `
         <div class="aparecer">
             <h2>Resultado:</h2>
-            <p>Média (μ): ${media.toFixed(2)}</p>
-            <p>Lambda (λ): ${lambda.toFixed(2)}</p>
-            <p>Probabilidade de X > a: ${probabilidade}%</p>
+            <p>Probabilidade de X > a:</p>
+            <p>${probabilidade}%</p>
         </div>
     `;
   document.getElementById('resultado').innerHTML = resultado;
@@ -35,23 +32,20 @@
 
    function menor(){
     a = parseFloat(document.getElementById('a').value);
-    tudo = parseFloat(document.getElementById('tudo').value);
     n = parseFloat(document.getElementById('n').value);
     let resultado = "";
     
-    if (isNaN(a)|| isNaN (tudo) || isNaN (n)) {
+    if (isNaN(a)|| isNaN (n)) {
       alert("Insira os valores");
       } else{
-    media =(tudo / n);
-    lambda = (1 / media);
+    lambda = (1 / n);
     probabilidade =arredondar2((1 - Math.exp(-lambda * a)) * 100);
 
     resultado = `
     <div class="aparecer">
         <h2>Resultado:</h2>
-        <p>Média (μ): ${media.toFixed(2)}</p>
-        <p>Lambda (λ): ${lambda.toFixed(2)}</p>
-        <p>Probabilidade de X > a: ${probabilidade}%</p>
+        <p>Probabilidade de X < a:</p>
+        <p>${probabilidade}%</p>
     </div>
     `;
 
@@ -60,19 +54,17 @@
   }
 
  function entre() {
-  const freq = parseFloat(document.getElementById("freq").value);
   const total = parseFloat(document.getElementById("total").value);
   const entreA = parseFloat(document.getElementById("entreA").value);
   const entreB = parseFloat(document.getElementById("entreB").value);
   let resultado2 = "";
 
-   if (isNaN(entreA)|| isNaN(entreB) || isNaN (total) || isNaN (freq)) {
+   if (isNaN(entreA)|| isNaN(entreB) || isNaN (total)) {
       alert("Insira os valores");}
     else if(entreA >= entreB){
         alert('O Valor A não pode ser menor ou igual ao Valor B!!')}
       else {
-        const media = total / freq;
-        const lambda = 1 / media;
+        const lambda = 1 / total;
         const Pma = Math.exp(-lambda * entreA);
         const Pmb = Math.exp(-lambda * entreB);
         const prob = arredondar2((Pma - Pmb) * 100);
@@ -80,11 +72,8 @@
         resultado2 = `
         <div class="aparecer">
           <h2>Resultado:</h2>
-          <p>Média (μ): ${media.toFixed(2)}</p>
-          <p>Lambda (λ): ${lambda.toFixed(2)}</p>
-          <p>Valor P(x > a): ${(Pma*100).toFixed(2)}%</p>
-          <p>Valor P(x > b): ${(Pmb*100).toFixed(2)}%</p>
-          <p>Probabilidade entre ${entreA} e ${entreB}: ${prob}%</p>
+          <p>Probabilidade entre ${entreA} e ${entreB}:</p>
+          <p>${prob}%</p>
         </div>
         `;
    console.log(prob);
